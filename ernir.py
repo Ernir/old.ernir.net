bf75e85ecd40b78076b8d54bf06cec0434ab813e
Ernir <e.ernir@gmail.com>
2014-03-13 19:07:11 +0000

    Added Myth-Weavers XML parser. Omitted: Error handling, so don't break it.

--- ernir.py
+++ ernir.py
@@ -1,7 +1,9 @@
 #/usr/lib/python2.7
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from xml_parse import Parser
+from errors import InvalidUsage
 
 app = Flask(__name__)
 
@@ -17,9 +19,30 @@ def ferilskra():
 def samband():
     return render_template("hafasamband.html", sitename=u"Hafa samband")
 
-@app.route("/kennsla/")
+@app.route("/forritun/") #TODO Do
+def forritun():
+    return None
+
+@app.route("/kennsla/") #TODO Do
 def kennsla():
     return None
+
+@app.route("/api/parseMW")
+def mw_parse():
+    sheet_id = request.args.get("id",0,type=int)
+
+    try:
+        data = Parser().get_mw_data(sheet_id)
+    except:
+        raise InvalidUsage("No Myth-Weavers sheet with the given ID was found", status_code=400)
+    return jsonify(data)
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ xml_parse.py
@@ -0,0 +1,56 @@
+from xml.dom.minidom import parseString
+import urllib2
+
+
+class Parser:
+
+    def get_mw_data(self, id):
+
+        ability_scores = self.parse_mw_sheet(id)
+
+        data = dict(status=200)
+        data["message"] = dict(ability_scores=ability_scores)
+
+        return data
+
+    def parse_mw_sheet(self, id):
+        xml_path = "http://www.myth-weavers.com/sheetview.php?sheetid=" + str(id)
+
+        xml_response = urllib2.urlopen(xml_path)  # TODO: Error handling. Right.
+        xml_string = xml_response.read()
+
+        # Stripping away all XML not between the first instance of the defined strings (inclusive)
+        new_xml_string = self.get_valid_block(xml_string, '<table id="statblock">', '</table>')
+
+        dom = parseString(new_xml_string)
+
+        ability_scores = []
+        # Extracting the values corresponding to the six ability scores
+        for sub_element in dom.getElementsByTagName("input"):
+            if sub_element.hasAttribute("class"):
+                if sub_element.attributes["class"].value == "mod" and "Mod" in sub_element.attributes["name"].value:
+                    ability_scores.append(int(sub_element.attributes["value"].value))
+
+        return ability_scores
+
+    def get_valid_block(self, xml, start_string, end_string):
+        # So, why am I extracting a sub-block rather than relying on the DOM parser?
+        # MW documents are not all well formed XML, it seems.
+        lines = xml.split("\n")
+
+        new_xml_string = ""
+        appending = False
+        for line in lines:
+            line = line.lstrip()
+            if not appending:
+                if start_string in line:
+                    appending = True
+            if appending:
+                new_xml_string += line
+
+            if appending and end_string in line:
+                break
+        return new_xml_string
+
+    def __init__(self):
+        pass
