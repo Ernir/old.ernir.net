9e7ccc8c589b62cdc8b5a10c871db3974665e63e
Ernir <e.ernir@gmail.com>
2015-08-17 16:13:51 +0000

    Google app information added to environment vars

--- updatedb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "User 'postgres' password:"
-pg_dump -Fc --no-acl --no-owner -h localhost -U postgres ernirnet > /home/ernir/Dropbox/Public/ernirnet.dump
-heroku run python manage.py migrate
-echo "Waiting for upload"
-sleep 5
-heroku pg:backups restore 'http://dl.dropboxusercontent.com/u/19444168/ernirnet.dump' DATABASE -a ernirnet --confirm ernirnet
