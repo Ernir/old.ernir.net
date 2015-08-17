a496abc37a75bdc8c85056ca2a288e1f094c55ed
Ernir <e.ernir@gmail.com>
2015-08-17 03:31:50 +0000

    Obvious procfile derp fixed.

--- ernirnet/wsgi.py
+++ ernirnet/wsgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
