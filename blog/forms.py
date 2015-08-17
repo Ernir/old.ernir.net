5d09aef2055cd87483c067f32eda44460fd7a606
Ernir <e.ernir@gmail.com>
2015-08-17 20:09:45 +0000

    Comments are now operational.

---
+++ blog/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.template.defaultfilters import mark_safe
+
+class CommentForm(forms.Form):
+
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={"class": "form-control"}
+        )
+    )
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 
-from blog.models import Entry
+from blog.models import Entry, Comment
+from blog.forms import CommentForm
+from datetime import datetime
 
 
 def index(request):
     """
 
-    The main page.
+    The list of all blogs.
     """
 
     entries = Entry.objects.all()
@@ -17,10 +19,22 @@ def index(request):
 def entry(request, blog_slug):
     """
 
-    Defines the page of a single maneuver.
-    Finds the maneuver based on its slugified name, passed in by man_slug.
+    Defines the page of a single blog entry.
+    Finds the entry based on its slugified title, passed in by blog_slug.
     """
 
     the_entry = get_object_or_404(Entry, slug=blog_slug)
+    comment_form = CommentForm()
 
-    return render(request, "individual_blog.html", {"entry": the_entry})
+    if request.method == "POST":
+        new_comment = Comment()
+        new_comment.content = request.POST["content"]
+        new_comment.author = request.user
+        new_comment.published = datetime.now()
+        new_comment.associated_with = the_entry
+        new_comment.save()
+
+    return render(request, "individual_blog.html", {
+        "entry": the_entry,
+        "comment_form": comment_form
+    })
