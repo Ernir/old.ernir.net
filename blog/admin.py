36c2eac0c471610eb8064141bf9b7afce390b953
Ernir <e.ernir@gmail.com>
2015-08-18 13:02:51 +0000

    The blog interface now uses a CKeditor.

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,22 @@
 from django.contrib import admin
 from blog.models import Entry, Comment, Tag
+from django.db import models
+from django import forms
 
 admin.site.register(Comment)
 
 
 class EntryAdmin(admin.ModelAdmin):
     exclude = ("slug", )  # these are auto-generated
+    formfield_overrides = {
+        models.TextField: {
+            'widget': forms.Textarea(attrs={'class': 'ckeditor'})
+        },
+    }
+
+    class Media:
+        css = {"all": ("admin_style.css",)}
+        js = ('//cdn.ckeditor.com/4.4.7/standard/ckeditor.js',)
 
 
 class TagAdmin(admin.ModelAdmin):
