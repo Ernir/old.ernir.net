20acd08dbaea2f330903386272c0bc02276c31fb
Ernir <e.ernir@gmail.com>
2015-09-20 14:03:39 +0000

    Navigation switched over to a tab format.

--- vanciantopsionics/views.py
+++ vanciantopsionics/views.py
@@ -14,17 +14,25 @@ def vtp_index(request):
     latest_file = vtp_files[0]
     older_files = vtp_files[1:]
 
+    all_chapters = Chapter.objects.values("title", "order")
+
+    category = "index"
+
     return render(request, "vtp_main.html", {
         "latest_file": latest_file,
-        "older_files": older_files
+        "older_files": older_files,
+        "chapters": all_chapters,
+        "category": category
     })
 
 
 def vtp_chapter(request, chapter_number):
     chapter = get_object_or_404(Chapter, order=chapter_number)
     all_chapters = Chapter.objects.values("title", "order")
+    category = "chapter"
 
     return render(request, "single_chapter.html", {
         "chapter": chapter,
-        "chapters": all_chapters
+        "chapters": all_chapters,
+        "category": category
     })
