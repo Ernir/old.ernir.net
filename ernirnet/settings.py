4c17b51ac82a3edc7edd4b9eea209e4060786a85
Ernir <e.ernir@gmail.com>
2020-05-20 00:19:49 +0200

    Use `python-dotenv` and `django-heroku` to manage the DB connection

--- ernirnet/settings.py
+++ ernirnet/settings.py
@@ -1,6 +1,9 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
+import django_heroku
+from dotenv import load_dotenv
+
+load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -9,8 +12,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY') or "not_at_all_secret"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DEBUG_MODE") or 1))
-
-ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -72,32 +73,17 @@ AUTHENTICATION_BACKENDS = (
 WSGI_APPLICATION = 'ernirnet.wsgi.application'
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-# Database
-DATABASES = {"default": {}}
-# Enable Connection Pooling
-DATABASES["default"]["ENGINE"] = "django_postgrespool"
-# Parse database configuration from $DATABASE_URL
-DATABASES['default'] = dj_database_url.config("ERNIRNET_DB_URL")
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Static asset configuration
-
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
+django_heroku.settings(locals())
