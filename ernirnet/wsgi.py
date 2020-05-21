1126f94a8527e76903f0cc3efd98a0356075f584
Ernir <e.ernir@gmail.com>
2020-05-21 12:08:23 +0200

    Fix the WSGI setup

--- ernirnet/wsgi.py
+++ ernirnet/wsgi.py
@@ -2,12 +2,11 @@
 WSGI config for ernirnet project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
+import os
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ernirnet.settings")
+
+application = get_wsgi_application()
